0425309faa4b9b26ca6d2694a11282d371b8e952
hadeer moustafa <71672005+hadeermoustafa118@users.noreply.github.com>
2023-06-25 14:28:48 +0300

    Update casheHelper.dart

--- lib/network/casheHelper.dart
+++ lib/network/casheHelper.dart
@@ -1,5 +1,6 @@
 import 'package:shared_preferences/shared_preferences.dart';
 class CashHelper {
+  int x = 0;
   static SharedPreferences? sharedPreferences;
   static init() async{
     sharedPreferences = await SharedPreferences.getInstance();
